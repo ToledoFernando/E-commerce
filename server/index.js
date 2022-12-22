c4f695f03c6e42667d6cc986e2b10ff4490dde12
ToledoFernando <elvioladorxx3@gmail.com>
2022-12-22 13:49:10 -0300

    xddd

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
-require('./db')
+// require('./db')
 const rutas = require('./routes/routes');
 const app = express();
 
