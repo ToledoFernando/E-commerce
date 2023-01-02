2c83ae8c4da5d77da22941e11069f4c00762089f
ToledoFernando <elvioladorxx3@gmail.com>
2023-01-02 03:31:08 -0300

    ...

---
+++ client/src/components/cardProducts/ProductsCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function ProductsCard({ producto }) {
+  const upper = (text) => {
+    const pri = text[0].toUpperCase();
+    return pri + text.slice(1, text.length);
+  };
+
+  return (
+    <div className="producto">
+      <img src={producto.productIMG} id={producto.imgid} />
+      <p className="marca">{producto.marca}</p>
+      <p>{upper(producto.name)}</p>
+      {producto.oferta ? (
+        <p>
+          Precio: <b>{producto.oferta}</b>
+        </p>
+      ) : (
+        <p>Precio: {producto.price}</p>
+      )}
+      <div className="detalle">
+        <button
+          className="verDetalle"
+          onClick={() => navigate(`detailP/${producto._id}`)}
+        >
+          Ver Detalle
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductsCard;
--- client/src/pages/products/products.jsx
+++ client/src/pages/products/products.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../store/action";
 import { useNavigate } from "react-router-dom";
 import "./products.scss";
+import ProductsCard from "../../components/cardProducts/ProductsCard";
 
 function Products() {
   const navigate = useNavigate();
@@ -13,41 +14,14 @@ function Products() {
     if (!productos.length) dispatch(getProducts());
   }, []);
 
-  const upper = (text) => {
-    const pri = text[0].toUpperCase();
-    // console.log();
-    return pri + text.slice(1, text.length);
-  };
-
   return (
     <div className="prductos">
       {!productos.length ? (
         <h1>Cargando</h1>
       ) : (
-        productos.map((producto) => {
-          return (
-            <div key={producto._id} className="producto">
-              <img src={producto.productIMG} id={producto.imgid} />
-              <p className="marca">{producto.marca}</p>
-              <p>{upper(producto.name)}</p>
-              {producto.oferta ? (
-                <p>
-                  Precio: <b>{producto.oferta}</b>
-                </p>
-              ) : (
-                <p>Precio: {producto.price}</p>
-              )}
-              <div className="detalle">
-                <button
-                  className="verDetalle"
-                  onClick={() => navigate(`detailP/${producto._id}`)}
-                >
-                  Ver Detalle
-                </button>
-              </div>
-            </div>
-          );
-        })
+        productos.map((producto) => (
+          <ProductsCard key={producto._id} producto={producto} />
+        ))
       )}
     </div>
   );
