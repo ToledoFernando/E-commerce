f22433d0e32daf1562ea87fa71ab74357cb4adfe
ToledoFernando <elvioladorxx3@gmail.com>
2023-01-10 20:33:50 -0300

    estilos al form del admin

--- client/src/pages/panelAdmin/productsAdmin/products.jsx
+++ client/src/pages/panelAdmin/productsAdmin/products.jsx
@@ -9,8 +9,8 @@ import {
 } from "../../../store/action";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./AddProduct.scss";
 import imgLoad from "../../../img/loadIMG.svg";
+import "./AddProduct.scss";
 
 const initial = {
   name: "",
@@ -29,7 +29,7 @@ function Products() {
   const dispatch = useDispatch();
   const [newProduct, setNewProduct] = useState(initial);
   const [img, setImg] = useState(initialIMG);
-  const [cargando, setCargando] = useState(false);
+  const [cargando, setCargando] = useState(true);
   const marcas = useSelector((state) => state.marcas);
   const categorys = useSelector((state) => state.categorys);
 
@@ -77,88 +77,113 @@ function Products() {
   }, []);
 
   return (
-    <div>
-      <h1>Agregar producto</h1>
+    <div className="addProduct">
+      <h1>
+        Agregar <span className="resaltar">producto</span>
+      </h1>
       <form onSubmit={handleSubmit}>
-        {cargando ? <img src={imgLoad} className="cargando" /> : null}
-        {!img.productIMG.length ? null : (
-          <>
-            <img
-              src={img.productIMG}
-              alt="nuevoProducto"
-              width="300"
-              height="300"
+        <div className="formAdd">
+          <label>
+            Seleccionar una imagen<span className="res">*</span>
+          </label>
+          <br />
+          <div className="upload">
+            <label htmlFor="img">Subir Imagen</label>
+            <input
+              disabled={img.productIMG.length}
+              accept="image/png,image/jpeg"
+              type="file"
+              id="img"
+              name="productIMG"
+              onChange={setImage}
             />
-            <button type="button" onClick={() => cambiarIMG(img.imgid)}>
-              Eliminar Imagen
-            </button>
-          </>
-        )}
-        <label>Seleccionar una imagen</label>
-        <br />
-        <input
-          disabled={img.productIMG.length}
-          accept="image/png,image/jpeg"
-          type="file"
-          name="productIMG"
-          onChange={setImage}
-        />
-        <br />
-        <label>Nombre del producto</label>
-        <input
-          onChange={handlechange}
-          value={newProduct.name}
-          type="text"
-          placeholder="name"
-          name="name"
-        />
-        <br />
-        <label>description</label>
-        <textarea
-          onChange={handlechange}
-          value={newProduct.description}
-          name="description"
-          placeholder="description"
-          cols="30"
-          rows="10"
-        ></textarea>
-        <br />
-        <label>Precio</label>
-        <input
-          onChange={handlechange}
-          value={newProduct.price}
-          type="number"
-          name="price"
-          placeholder="Precio"
-        />
-        <br />
-        <p>Categorias</p>
-        <select name="category" onChange={handleSelected}>
-          {categorys?.map((category) => (
-            <option key={category.id} value={category.id}>
-              {category.name}
-            </option>
-          ))}
-        </select>
-        <p>Marcas</p>
-        <select name="marcaId" onChange={handlechange}>
-          {marcas?.map((marca) => (
-            <option key={marca.id} value={marca.id}>
-              {marca.name}
-            </option>
-          ))}
-        </select>
-        <label>Oferta</label>
-        <input
-          onChange={handlechange}
-          value={newProduct.oferta}
-          type="number"
-          placeholder="Si no tiene oferta dejar vacio"
-          name="oferta"
-        />
-        <br />
-        <br />
-        <button>Subir Producto</button>
+          </div>
+          <br />
+          <label>
+            Nombre del producto<span className="res">*</span>
+          </label>
+          <input
+            onChange={handlechange}
+            value={newProduct.name}
+            type="text"
+            placeholder="Nombre del Producto"
+            name="name"
+          />
+          <br />
+          <label>
+            Description<span className="res">*</span>
+          </label>
+          <textarea
+            className="desc"
+            onChange={handlechange}
+            value={newProduct.description}
+            name="description"
+            placeholder="Description"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <br />
+          <label>
+            Precio<span className="res">*</span>
+          </label>
+          <input
+            onChange={handlechange}
+            value={newProduct.price}
+            type="number"
+            name="price"
+            placeholder="Precio"
+          />
+          <br />
+          <p>
+            Categorias<span className="res">*</span>
+          </p>
+          <select name="category" onChange={handleSelected}>
+            {categorys?.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+          <p>
+            Marcas<span className="res">*</span>
+          </p>
+          <select name="marcaId" onChange={handlechange}>
+            {marcas?.map((marca) => (
+              <option key={marca.id} value={marca.id}>
+                {marca.name}
+              </option>
+            ))}
+          </select>
+          <label>
+            Oferta<span className="res">*</span>
+          </label>
+          <input
+            onChange={handlechange}
+            value={newProduct.oferta}
+            type="number"
+            placeholder="Si no tiene oferta dejar vacio"
+            name="oferta"
+          />
+          <br />
+          <br />
+          <button>Subir Producto</button>
+        </div>
+        <div className="loadIMG">
+          {cargando ? <img src={imgLoad} className="cargando" /> : null}
+          {!img.productIMG.length ? null : (
+            <>
+              <img
+                src={img.productIMG}
+                alt="nuevoProducto"
+                width="300"
+                height="300"
+              />
+              <button type="button" onClick={() => cambiarIMG(img.imgid)}>
+                Eliminar Imagen
+              </button>
+            </>
+          )}
+        </div>
       </form>
     </div>
   );
