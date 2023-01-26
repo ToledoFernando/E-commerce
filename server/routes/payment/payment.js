4afb3fcb5f315f0f17bf4ab67842599dfdf79570
toledo <toledof764@gmail.com>
2023-01-26 13:31:31 -0300

    new routes

--- server/routes/payment/payment.js
+++ server/routes/payment/payment.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
 const { paymentID } = require("./payment.controllers");
+const verifyToken = require("../../jwt/VerifyToken");
 
 const route = Router();
 
-route.post("/preferensID", paymentID);
+route.post("/preferensID", verifyToken, paymentID);
+
+route.get("/getAllPay", verifyToken);
+
+route.put("/updatePay", verifyToken);
 
 module.exports = route;
